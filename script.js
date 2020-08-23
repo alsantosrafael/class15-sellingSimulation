157e1b3539b23600c9b1eec1341d9bba06df627b
Rafael Almeida <rafael.profeng@gmail.com>
2020-08-23 19:27:47 -0300

    New commit. Added new features.

--- script.js
+++ script.js
@@ -18,3 +18,42 @@ const rl = readline.createInterface({
 /**
  * Escreva seu código aqui embaixo;
  */
+const chalk = require('chalk');
+const vendinhaDeFrutas = [
+  {
+    nome: 'uva',
+    preco: 300,
+    quant: 5
+  },
+  {
+    nome: 'mamao papaya',
+    preco: 400,
+    quant: 3
+  },
+  {
+    nome: 'banana',
+    preco: 350,
+    quant: 10
+  },
+  {
+    nome: 'maca',
+    preco: 400,
+    quant: 20
+  }
+]
+
+rl.question('Olá, querido cliente! Qual fruta você gostaria de comprar?', 
+(resposta) => {
+  let flag = false;
+  for(let i = 0; i < vendinhaDeFrutas.length; i++){
+    if(resposta === vendinhaDeFrutas[i].nome){
+      console.log(chalk.greenBright(`Achei aqui o(a) ${resposta}!`))
+      flag = true
+      rl.close()
+    }
+  } if(flag === false){
+    console.log(chalk.redBright('Não consegui achar ' + `${resposta}!`))
+    rl.close()
+  }
+  
+});
